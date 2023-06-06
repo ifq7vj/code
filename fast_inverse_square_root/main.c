01f66c7fcae417611fc0a1fef1a659b78fa09d8a
ifq7vj <ifq7vj9@gmail.com>
2023-06-06 12:28:26 +0000

    add: fast_inverse_square_root

---
+++ fast_inverse_square_root/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+float rsqrt(float);
+
+int main(void) {
+    float x;
+    scanf("%f", &x);
+    float y = rsqrt(x);
+    printf("%f\n", y);
+    return 0;
+}
+
+float rsqrt(float x) {
+    const float h = x * .5f;
+    long i = *(long*)&x;
+    i = 0x5f3759df - (i >> 1);
+    x = *(float*)&i;
+    x *= 1.5f - h * x * x;
+    x *= 1.5f - h * x * x;
+    return x;
+}
