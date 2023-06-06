071e0ecd1b8e7cb8cc92551e976c44d97ea0d94d
ifq7vj <ifq7vj9@gmail.com>
2023-06-06 12:28:06 +0000

    add: monte_carlo_method

---
+++ monte_carlo_method/main.c
@@ -0,0 +1,21 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) {
+    srand((unsigned int)time(NULL));
+    const double r = 1. / ((long)RAND_MAX + 1);
+    int n;
+    scanf("%d", &n);
+    int a = 0;
+
+    for (int i = 0; i < n; i++) {
+        double x = rand() * r;
+        double y = rand() * r;
+        a += hypot(x, y) <= 1;
+    }
+
+    printf("%lf\n", (double)a * 4 / n);
+    return 0;
+}
