bacfb69eeb77fe877e359fd8a412483b91b97b4c
ifq7vj <ifq7vj9@gmail.com>
2023-06-27 07:56:12 +0000

    refactor: if-else statement

--- calculator/main.c
+++ calculator/main.c
@@ -3,19 +3,67 @@
 
 int ptr;
 
-bool consume(int);
-int number(void);
-
 int expr(void);    // expr = trem ("+" trem | "-" trem)*
 int trem(void);    // trem = unary ("*" unary | "/" unary)*
 int unary(void);   // unary = ("+" | "-")? factor
 int factor(void);  // factor = number | "(" expr ")"
+
+bool consume(int);
+int number(void);
 
 int main(void) {
     ptr = getchar();
     int num = expr();
     printf("%d\n", num);
     return 0;
+}
+
+int expr(void) {
+    int num = trem();
+
+    while (true) {
+        if (consume('+')) {
+            num += trem();
+        } else if (consume('-')) {
+            num -= trem();
+        } else {
+            return num;
+        }
+    }
+}
+
+int trem(void) {
+    int num = unary();
+
+    while (true) {
+        if (consume('*')) {
+            num *= unary();
+        } else if (consume('/')) {
+            num /= unary();
+        } else {
+            return num;
+        }
+    }
+}
+
+int unary(void) {
+    if (consume('+')) {
+        return +factor();
+    } else if (consume('-')) {
+        return -factor();
+    } else {
+        return factor();
+    }
+}
+
+int factor(void) {
+    if (consume('(')) {
+        int num = expr();
+        consume(')');
+        return num;
+    }
+
+    return number();
 }
 
 bool consume(int op) {
@@ -44,62 +92,4 @@ int number(void) {
     }
 
     return num;
-}
-
-int expr(void) {
-    int num = trem();
-
-    while (true) {
-        if (consume('+')) {
-            num += trem();
-            continue;
-        }
-
-        if (consume('-')) {
-            num -= trem();
-            continue;
-        }
-
-        return num;
-    }
-}
-
-int trem(void) {
-    int num = unary();
-
-    while (true) {
-        if (consume('*')) {
-            num *= unary();
-            continue;
-        }
-
-        if (consume('/')) {
-            num /= unary();
-            continue;
-        }
-
-        return num;
-    }
-}
-
-int unary(void) {
-    if (consume('+')) {
-        return +factor();
-    }
-
-    if (consume('-')) {
-        return -factor();
-    }
-
-    return factor();
-}
-
-int factor(void) {
-    if (consume('(')) {
-        int num = expr();
-        consume(')');
-        return num;
-    }
-
-    return number();
 }
